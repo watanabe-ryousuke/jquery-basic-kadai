d8bd8f8d84086a64827f47c440b3d9fa53caedc8
watanabe-ryousuke <146714563+watanabe-ryousuke@users.noreply.github.com>
2023-11-01 23:29:35 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('#change-color').on('click', function() {
+    $('p').css('color', 'red');
+  });
+
+  $('#change-text').on('click', function() {
+    $('p').text('Hello!');
+  });
+
+  $('#fade-out').on('click', function() {
+    $('p').fadeOut();
+  });
+
+  $('#fade-in').on('click', function() {
+    $('p').fadeIn();
+  });
+});
